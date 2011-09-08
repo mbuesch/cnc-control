f5c50c8ef0e1c387870da5b8cc1fcb5853e19546
Michael Buesch <m@bues.ch>
2011-09-08 18:39:10 +0200

    Install driver with python distutils
    Signed-off-by: Michael Buesch <m@bues.ch>

---
+++ driver/setup_driver.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+	name		= "cnccontrol-driver",
+	description	= "CNC-Control device driver",
+	author		= "Michael Buesch",
+	author_email	= "m@bues.ch",
+	py_modules	= [ "cnccontrol_driver", ],
+)
