17f6ac1dd525c8cbe5b9deac10481aa5fc817ae9
Michael Buesch <m@bues.ch>
2011-09-13 18:33:46 +0200

    Convert HAL driver to OOP
    Signed-off-by: Michael Buesch <m@bues.ch>

--- driver/emc2hal_cnccontrol.py
+++ driver/emc2hal_cnccontrol.py
@@ -18,28 +18,6 @@ from hal import HAL_IN, HAL_OUT, HAL_RO, HAL_RW
 from cnccontrol_driver import *
 
 
-class Context:
-	def __init__(self, h, cncc):
-		self.h = h
-		self.cncc = cncc
-
-		self.foBalance = ValueBalance(h,
-				scalePin="feed-override.scale",
-				incPin="feed-override.inc",
-				decPin="feed-override.dec",
-				feedbackPin="feed-override.value")
-
-		self.incJogPlus = {}
-		self.incJogMinus = {}
-		for ax in ALL_AXES:
-			self.incJogPlus[ax] = BitPoke(h, "jog.%s.inc-plus" % ax)
-			self.incJogMinus[ax] = BitPoke(h, "jog.%s.inc-minus" % ax)
-
-		self.programStop = BitPoke(h, "program.stop", cycleMsec=100)
-
-		self.spindleStart = BitPoke(h, "spindle.start")
-		self.spindleStop = BitPoke(h, "spindle.stop")
-
 class BitPoke:
 	def __init__(self, h, pin, cycleMsec=15):
 		self.h = h
@@ -94,229 +72,257 @@ class ValueBalance:
 		else:
 			self.incBit.startDuty()
 
-def resetOutputPins(ctx):
-	h = ctx.h
+class CNCControlHAL(CNCControl):
+	def __init__(self, halName="cnccontrol"):
+		CNCControl.__init__(self, verbose=True)
 
-	# Jogging
-	h["jog.velocity"] = 0
-	for ax in ALL_AXES:
-		h["jog.%s.minus" % ax] = 0
-		h["jog.%s.plus" % ax] = 0
-		h["jog.%s.inc" % ax] = 0
-		h["jog.%s.inc-plus" % ax] = 0
-		h["jog.%s.inc-minus" % ax] = 0
+		self.h = hal.component(halName)
+		self.__createHalPins()
+		self.h.ready()
 
-	# Master spindle
-	h["spindle.forward"] = 0
-	h["spindle.reverse"] = 0
-	h["spindle.start"] = 0
-	h["spindle.stop"] = 0
+		self.foBalance = ValueBalance(self.h,
+				scalePin="feed-override.scale",
+				incPin="feed-override.inc",
+				decPin="feed-override.dec",
+				feedbackPin="feed-override.value")
 
-	# Feed override
-	h["feed-override.dec"] = 0
-	h["feed-override.inc"] = 0
-	h["feed-override.scale"] = 0
+		self.incJogPlus = {}
+		self.incJogMinus = {}
+		for ax in ALL_AXES:
+			self.incJogPlus[ax] = BitPoke(self.h, "jog.%s.inc-plus" % ax)
+			self.incJogMinus[ax] = BitPoke(self.h, "jog.%s.inc-minus" % ax)
 
-	# Program control
-	h["program.stop"] = 0
+		self.programStop = BitPoke(self.h, "program.stop", cycleMsec=100)
 
-def updatePins(ctx):
-	h = ctx.h
-	cncc = ctx.cncc
+		self.spindleStart = BitPoke(self.h, "spindle.start")
+		self.spindleStop = BitPoke(self.h, "spindle.stop")
 
-	cncc.setEstopState(h["machine.estop.active"])
-	if not h["machine.on"] or\
-	   not cncc.deviceIsTurnedOn():
-		resetOutputPins(ctx)
-		return
-
-	# Halt the program, if requested
-	if cncc.haveMotionHaltRequest():
-		ctx.programStop.startDuty()
-	ctx.programStop.update()
-
-	# Update master spindle state
-	if ctx.spindleStart.update() and\
-	   ctx.spindleStop.update() and\
-	   h["machine.mode.jog"]:
-		direction = cncc.getSpindleCommand()
-		if direction < 0: # backward
-			if not h["spindle.runs-bwd"]:
-				h["spindle.forward"] = 0
-				h["spindle.reverse"] = 1
-				ctx.spindleStart.startDuty()
-		elif direction > 0: # forward
-			if not h["spindle.runs-fwd"]:
-				h["spindle.forward"] = 1
-				h["spindle.reverse"] = 0
-				ctx.spindleStart.startDuty()
-		else: # stop
-			if h["spindle.runs-fwd"] or h["spindle.runs-bwd"]:
-				h["spindle.forward"] = 0
-				h["spindle.reverse"] = 0
-				ctx.spindleStop.startDuty()
-	if h["spindle.runs-bwd"]:
-		cncc.setSpindleState(-1)
-	elif h["spindle.runs-fwd"]:
-		cncc.setSpindleState(1)
-	else:
-		cncc.setSpindleState(0)
-
-	# Update feed override state
-	foValue = cncc.getFeedOverrideState(h["feed-override.min-value"],
-					    h["feed-override.max-value"])
-	ctx.foBalance.balance(foValue)
-	cncc.setFeedOverrideState(h["feed-override.value"] * 100)
-
-	# Update jog states
-	velocity = h["jog.velocity-rapid"]
-	jogParams = {}
-	for ax in ALL_AXES:
-		if not h["axis.%s.enable" % ax]:
-			continue
-		(direction, incremental, vel) = cncc.getJogState(ax)
-		if not equal(direction, 0.0):
-			if vel < 0: # Rapid move
-				vel = h["jog.velocity-rapid"]
-			velocity = min(velocity, vel)
-		jogParams[ax] = (direction, incremental)
-	h["jog.velocity"] = velocity
-	for ax in jogParams:
-		ctx.incJogPlus[ax].update()
-		ctx.incJogMinus[ax].update()
-		if not h["machine.mode.jog"]:
-			h["jog.%s.minus" % ax] = 0
-			h["jog.%s.plus" % ax] = 0
-			continue
-		(direction, incremental) = jogParams[ax]
-		if incremental and not equal(direction, 0.0):
-			h["jog.%s.minus" % ax] = 0
-			h["jog.%s.plus" % ax] = 0
-			if not ctx.incJogPlus[ax].state() and\
-			   not ctx.incJogMinus[ax].state():
-				h["jog.%s.inc" % ax] = abs(direction)
-				if direction > 0:
-					ctx.incJogPlus[ax].startDuty()
-				else:
-					ctx.incJogMinus[ax].startDuty()
-		else:
-			if direction < -0.00001: # backward
-				h["jog.%s.plus" % ax] = 0
-				h["jog.%s.minus" % ax] = 1
-			elif direction > 0.00001: # forward
-				h["jog.%s.minus" % ax] = 0
-				h["jog.%s.plus" % ax] = 1
-			else: # stop
-				h["jog.%s.minus" % ax] = 0
-				h["jog.%s.plus" % ax] = 0
-
-	# Update axis states
-	g53coords = cncc.wantG53Coords()
-	for ax in ALL_AXES:
-		if not h["axis.%s.enable" % ax]:
-			continue
-		if g53coords:
-			pos = h["axis.%s.pos.machine-coords" % ax]
-		else:
-			pos = h["axis.%s.pos.user-coords" % ax]
-		cncc.setAxisPosition(ax, pos)
-
-def deviceInitialize(h, cncc):
-	# CNC-Control USB device connected. Initialize it.
-	cncc.deviceReset()
-	cncc.setDebugging(h["config.debug"], h["config.usblogmsg"])
-	cncc.setTwohandEnabled(h["config.twohand"])
-	for i in range(0, ControlMsgSetincrement.MAX_INDEX + 1):
-		cncc.setIncrementAtIndex(i, h["jog.increment.%d" % i])
-	axes = map(lambda ax: ax if h["axis.%s.enable" % ax] else "",
-		   ALL_AXES)
-	cncc.setEnabledAxes(filter(None, axes))
-
-def createPins(h):
-	# Device config
-	h.newparam("config.twohand", HAL_BIT, HAL_RW)
-	h.newparam("config.debug", HAL_U32, HAL_RW)
-	h.newparam("config.debugperf", HAL_BIT, HAL_RW)
-	h.newparam("config.usblogmsg", HAL_BIT, HAL_RW)
-
-	# Machine state
-	h.newpin("machine.on", HAL_BIT, HAL_IN)
-	h.newpin("machine.estop.active", HAL_BIT, HAL_IN)
-	h.newpin("machine.mode.jog", HAL_BIT, HAL_IN)
-	h.newpin("machine.mode.mdi", HAL_BIT, HAL_IN)
-	h.newpin("machine.mode.auto", HAL_BIT, HAL_IN)
-
-	# Axis
-	for ax in ALL_AXES:
-		h.newparam("axis.%s.enable" % ax, HAL_BIT, HAL_RW)
-		h.newpin("axis.%s.pos.machine-coords" % ax, HAL_FLOAT, HAL_IN)
-		h.newpin("axis.%s.pos.user-coords" % ax, HAL_FLOAT, HAL_IN)
-
-	# Jogging
-	h.newpin("jog.velocity", HAL_FLOAT, HAL_OUT)
-	h.newparam("jog.velocity-rapid", HAL_FLOAT, HAL_RW)
-	for i in range(0, ControlMsgSetincrement.MAX_INDEX + 1):
-		h.newparam("jog.increment.%d" % i, HAL_FLOAT, HAL_RW)
-	for ax in ALL_AXES:
-		h.newpin("jog.%s.minus" % ax, HAL_BIT, HAL_OUT)
-		h.newpin("jog.%s.plus" % ax, HAL_BIT, HAL_OUT)
-		h.newpin("jog.%s.inc" % ax, HAL_FLOAT, HAL_OUT)
-		h.newpin("jog.%s.inc-plus" % ax, HAL_BIT, HAL_OUT)
-		h.newpin("jog.%s.inc-minus" % ax, HAL_BIT, HAL_OUT)
-
-	# Master spindle
-	h.newpin("spindle.runs-bwd", HAL_BIT, HAL_IN)
-	h.newpin("spindle.runs-fwd", HAL_BIT, HAL_IN)
-	h.newpin("spindle.forward", HAL_BIT, HAL_OUT)
-	h.newpin("spindle.reverse", HAL_BIT, HAL_OUT)
-	h.newpin("spindle.start", HAL_BIT, HAL_OUT)
-	h.newpin("spindle.stop", HAL_BIT, HAL_OUT)
-
-	# Feed override
-	h.newpin("feed-override.scale", HAL_FLOAT, HAL_OUT)
-	h.newpin("feed-override.dec", HAL_BIT, HAL_OUT)
-	h.newpin("feed-override.inc", HAL_BIT, HAL_OUT)
-	h.newpin("feed-override.value", HAL_FLOAT, HAL_IN)
-	h.newparam("feed-override.min-value", HAL_FLOAT, HAL_RW)
-	h.newparam("feed-override.max-value", HAL_FLOAT, HAL_RW)
-
-	# Program control
-	h.newpin("program.stop", HAL_BIT, HAL_OUT)
-
-def checkEMC():
-	try:
-		os.stat("/tmp/emc.lock")
-	except (OSError), e:
-		print "CNC-Control: EMC2 doesn't seem to be running"
-		raise KeyboardInterrupt
-	return True
-
-def pingDevice(ctx):
-	for i in range(0, 3):
-		if ctx.cncc.deviceAppPing():
-			break
-	else:
-		CNCCFatal.error("Failed to ping the device")
-
-def eventLoop(ctx):
-	avgRuntime = 0
-	lastRuntimePrint = -1
-	lastPing = -1
-	timeDebug = bool(ctx.h["config.debugperf"])
-	while checkEMC():
-		start = datetime.now()
+	def __checkEMC(self):
 		try:
-			if start.second != lastPing:
-				lastPing = start.second
-				pingDevice(ctx)
-			ctx.cncc.eventWait()
-			# Update pins, even if we didn't receive an event.
-			updatePins(ctx)
-		except (CNCCFatal), e:
-			raise # Drop out of event loop and re-probe device.
-		except (CNCCException), e:
-			print "CNC-Control error: " + str(e)
-		if timeDebug:
+			os.stat("/tmp/emc.lock")
+		except (OSError), e:
+			print "CNC-Control: EMC2 doesn't seem to be running"
+			raise KeyboardInterrupt
+		return True
+
+	def __createHalPins(self):
+		h = self.h
+
+		# Device config
+		h.newparam("config.twohand", HAL_BIT, HAL_RW)
+		h.newparam("config.debug", HAL_U32, HAL_RW)
+		h.newparam("config.debugperf", HAL_BIT, HAL_RW)
+		h.newparam("config.usblogmsg", HAL_BIT, HAL_RW)
+
+		# Machine state
+		h.newpin("machine.on", HAL_BIT, HAL_IN)
+		h.newpin("machine.estop.active", HAL_BIT, HAL_IN)
+		h.newpin("machine.mode.jog", HAL_BIT, HAL_IN)
+		h.newpin("machine.mode.mdi", HAL_BIT, HAL_IN)
+		h.newpin("machine.mode.auto", HAL_BIT, HAL_IN)
+
+		# Axis
+		for ax in ALL_AXES:
+			h.newparam("axis.%s.enable" % ax, HAL_BIT, HAL_RW)
+			h.newpin("axis.%s.pos.machine-coords" % ax, HAL_FLOAT, HAL_IN)
+			h.newpin("axis.%s.pos.user-coords" % ax, HAL_FLOAT, HAL_IN)
+
+		# Jogging
+		h.newpin("jog.velocity", HAL_FLOAT, HAL_OUT)
+		h.newparam("jog.velocity-rapid", HAL_FLOAT, HAL_RW)
+		for i in range(0, ControlMsgSetincrement.MAX_INDEX + 1):
+			h.newparam("jog.increment.%d" % i, HAL_FLOAT, HAL_RW)
+		for ax in ALL_AXES:
+			h.newpin("jog.%s.minus" % ax, HAL_BIT, HAL_OUT)
+			h.newpin("jog.%s.plus" % ax, HAL_BIT, HAL_OUT)
+			h.newpin("jog.%s.inc" % ax, HAL_FLOAT, HAL_OUT)
+			h.newpin("jog.%s.inc-plus" % ax, HAL_BIT, HAL_OUT)
+			h.newpin("jog.%s.inc-minus" % ax, HAL_BIT, HAL_OUT)
+
+		# Master spindle
+		h.newpin("spindle.runs-bwd", HAL_BIT, HAL_IN)
+		h.newpin("spindle.runs-fwd", HAL_BIT, HAL_IN)
+		h.newpin("spindle.forward", HAL_BIT, HAL_OUT)
+		h.newpin("spindle.reverse", HAL_BIT, HAL_OUT)
+		h.newpin("spindle.start", HAL_BIT, HAL_OUT)
+		h.newpin("spindle.stop", HAL_BIT, HAL_OUT)
+
+		# Feed override
+		h.newpin("feed-override.scale", HAL_FLOAT, HAL_OUT)
+		h.newpin("feed-override.dec", HAL_BIT, HAL_OUT)
+		h.newpin("feed-override.inc", HAL_BIT, HAL_OUT)
+		h.newpin("feed-override.value", HAL_FLOAT, HAL_IN)
+		h.newparam("feed-override.min-value", HAL_FLOAT, HAL_RW)
+		h.newparam("feed-override.max-value", HAL_FLOAT, HAL_RW)
+
+		# Program control
+		h.newpin("program.stop", HAL_BIT, HAL_OUT)
+
+	def __resetHalOutputPins(self):
+		h = self.h
+
+		# Jogging
+		h["jog.velocity"] = 0
+		for ax in ALL_AXES:
+			h["jog.%s.minus" % ax] = 0
+			h["jog.%s.plus" % ax] = 0
+			h["jog.%s.inc" % ax] = 0
+			h["jog.%s.inc-plus" % ax] = 0
+			h["jog.%s.inc-minus" % ax] = 0
+
+		# Master spindle
+		h["spindle.forward"] = 0
+		h["spindle.reverse"] = 0
+		h["spindle.start"] = 0
+		h["spindle.stop"] = 0
+
+		# Feed override
+		h["feed-override.dec"] = 0
+		h["feed-override.inc"] = 0
+		h["feed-override.scale"] = 0
+
+		# Program control
+		h["program.stop"] = 0
+
+	def __deviceInitialize(self):
+		# CNC-Control USB device connected. Initialize it.
+		h = self.h
+		self.deviceReset()
+		self.setDebugging(h["config.debug"], h["config.usblogmsg"])
+		self.setTwohandEnabled(h["config.twohand"])
+		for i in range(0, ControlMsgSetincrement.MAX_INDEX + 1):
+			self.setIncrementAtIndex(i, h["jog.increment.%d" % i])
+		axes = map(lambda ax: ax if h["axis.%s.enable" % ax] else "",
+			   ALL_AXES)
+		self.setEnabledAxes(filter(None, axes))
+
+	def __pingDevice(self):
+		for i in range(0, 3):
+			if self.deviceAppPing():
+				break
+		else:
+			CNCCFatal.error("Failed to ping the device")
+
+	def __updatePins(self):
+		h = self.h
+
+		self.setEstopState(h["machine.estop.active"])
+		if not h["machine.on"] or\
+		   not self.deviceIsTurnedOn():
+			self.__resetHalOutputPins()
+			return
+
+		# Halt the program, if requested
+		if self.haveMotionHaltRequest():
+			self.programStop.startDuty()
+		self.programStop.update()
+
+		# Update master spindle state
+		if self.spindleStart.update() and\
+		   self.spindleStop.update() and\
+		   h["machine.mode.jog"]:
+			direction = self.getSpindleCommand()
+			if direction < 0: # backward
+				if not h["spindle.runs-bwd"]:
+					h["spindle.forward"] = 0
+					h["spindle.reverse"] = 1
+					self.spindleStart.startDuty()
+			elif direction > 0: # forward
+				if not h["spindle.runs-fwd"]:
+					h["spindle.forward"] = 1
+					h["spindle.reverse"] = 0
+					self.spindleStart.startDuty()
+			else: # stop
+				if h["spindle.runs-fwd"] or h["spindle.runs-bwd"]:
+					h["spindle.forward"] = 0
+					h["spindle.reverse"] = 0
+					self.spindleStop.startDuty()
+		if h["spindle.runs-bwd"]:
+			self.setSpindleState(-1)
+		elif h["spindle.runs-fwd"]:
+			self.setSpindleState(1)
+		else:
+			self.setSpindleState(0)
+
+		# Update feed override state
+		foValue = self.getFeedOverrideState(h["feed-override.min-value"],
+						    h["feed-override.max-value"])
+		self.foBalance.balance(foValue)
+		self.setFeedOverrideState(h["feed-override.value"] * 100)
+
+		# Update jog states
+		velocity = h["jog.velocity-rapid"]
+		jogParams = {}
+		for ax in ALL_AXES:
+			if not h["axis.%s.enable" % ax]:
+				continue
+			(direction, incremental, vel) = self.getJogState(ax)
+			if not equal(direction, 0.0):
+				if vel < 0: # Rapid move
+					vel = h["jog.velocity-rapid"]
+				velocity = min(velocity, vel)
+			jogParams[ax] = (direction, incremental)
+		h["jog.velocity"] = velocity
+		for ax in jogParams:
+			self.incJogPlus[ax].update()
+			self.incJogMinus[ax].update()
+			if not h["machine.mode.jog"]:
+				h["jog.%s.minus" % ax] = 0
+				h["jog.%s.plus" % ax] = 0
+				continue
+			(direction, incremental) = jogParams[ax]
+			if incremental and not equal(direction, 0.0):
+				h["jog.%s.minus" % ax] = 0
+				h["jog.%s.plus" % ax] = 0
+				if not self.incJogPlus[ax].state() and\
+				   not self.incJogMinus[ax].state():
+					h["jog.%s.inc" % ax] = abs(direction)
+					if direction > 0:
+						self.incJogPlus[ax].startDuty()
+					else:
+						self.incJogMinus[ax].startDuty()
+			else:
+				if direction < -0.00001: # backward
+					h["jog.%s.plus" % ax] = 0
+					h["jog.%s.minus" % ax] = 1
+				elif direction > 0.00001: # forward
+					h["jog.%s.minus" % ax] = 0
+					h["jog.%s.plus" % ax] = 1
+				else: # stop
+					h["jog.%s.minus" % ax] = 0
+					h["jog.%s.plus" % ax] = 0
+
+		# Update axis states
+		g53coords = self.wantG53Coords()
+		for ax in ALL_AXES:
+			if not h["axis.%s.enable" % ax]:
+				continue
+			if g53coords:
+				pos = h["axis.%s.pos.machine-coords" % ax]
+			else:
+				pos = h["axis.%s.pos.user-coords" % ax]
+			self.setAxisPosition(ax, pos)
+
+	def __eventLoop(self):
+		avgRuntime = 0
+		lastRuntimePrint = -1
+		lastPing = -1
+		timeDebug = bool(self.h["config.debugperf"])
+		while self.__checkEMC():
+			start = datetime.now()
+			try:
+				if start.second != lastPing:
+					lastPing = start.second
+					self.__pingDevice()
+				self.eventWait()
+				# Update pins, even if we didn't receive an event.
+				self.__updatePins()
+			except (CNCCFatal), e:
+				raise # Drop out of event loop and re-probe device.
+			except (CNCCException), e:
+				print "CNC-Control error: " + str(e)
+			if not timeDebug:
+				continue
 			runtime = (datetime.now() - start).microseconds
 			avgRuntime = (avgRuntime + runtime) // 2
 			if start.second != lastRuntimePrint:
@@ -324,23 +330,20 @@ def eventLoop(ctx):
 				print "Average event loop runtime = %.1f milliseconds" %\
 					(float(avgRuntime) / 1000)
 
-def probeLoop(h):
-	cncc = CNCControl(verbose=True)
-	h.ready()
-	ctx = Context(h, cncc)
-	resetOutputPins(ctx)
-	while checkEMC():
-		try:
-			if cncc.probe():
-				deviceInitialize(h, cncc)
-				eventLoop(ctx)
-			else:
-				time.sleep(0.2)
-		except (CNCCFatal), e:
-			print "CNC-Control fatal error: " + str(e)
-		except (CNCCException), e:
-			print "CNC-Control error: " + str(e)
-		resetOutputPins(ctx)
+	def probeLoop(self):
+		self.__resetHalOutputPins()
+		while self.__checkEMC():
+			try:
+				if self.probe():
+					self.__deviceInitialize()
+					self.__eventLoop()
+				else:
+					time.sleep(0.2)
+			except (CNCCFatal), e:
+				print "CNC-Control fatal error: " + str(e)
+			except (CNCCException), e:
+				print "CNC-Control error: " + str(e)
+			self.__resetHalOutputPins()
 
 def main():
 	try:
@@ -348,9 +351,8 @@ def main():
 			os.nice(-5)
 		except (OSError), e:
 			print "WARNING: Failed to renice cnccontrol HAL module:", str(e)
-		h = hal.component("cnccontrol")
-		createPins(h)
-		probeLoop(h)
+		cncc = CNCControlHAL()
+		cncc.probeLoop()
 	except (CNCCException), e:
 		print "CNC-Control: Unhandled exception: " + str(e)
 		return 1
