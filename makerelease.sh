774c71ed7d4235eb67021b4de440ed9ba77f58b1
Michael Buesch <m@bues.ch>
2012-02-20 18:10:10 +0100

    Rewrite release script
    Signed-off-by: Michael Buesch <m@bues.ch>

--- makerelease.sh
+++ makerelease.sh
@@ -1,63 +1,30 @@
-#!/bin/bash
-set -e
+#!/bin/sh
 
-project="cnc-control"
+srcdir="$(dirname "$0")"
+[ "$(echo "$srcdir" | cut -c1)" = '/' ] || srcdir="$PWD/$srcdir"
 
-basedir="$(dirname "$0")"
-[ "${basedir:0:1}" = "/" ] || basedir="$PWD/$basedir"
+die() { echo "$*"; exit 1; }
 
-origin="$basedir"
-tmpdir="/tmp"
+# Import the makerelease.lib
+for path in $(echo "$PATH" | tr ':' ' '); do
+	[ -f "$MAKERELEASE_LIB" ] && break
+	MAKERELEASE_LIB="$path/makerelease.lib"
+done
+[ -f "$MAKERELEASE_LIB" ] && . "$MAKERELEASE_LIB" || die "makerelease.lib not found."
 
-do_git_tag=1
-[ "$1" = "--notag" ] && do_git_tag=0
+hook_get_version()
+{
+	local file="$1/firmware/cpu-firmware/main.h"
+	local major="$(cat "$file" | grep -e 'VERSION_MAJOR' | head -n1 | cut -f2)"
+	local minor="$(cat "$file" | grep -e 'VERSION_MINOR' | head -n1 | cut -f2)"
+	version="$major.$minor"
+}
 
-major="$(cat $origin/firmware/cpu-firmware/main.h | grep -e 'VERSION_MAJOR' | head -n1 | cut -f2)"
-minor="$(cat $origin/firmware/cpu-firmware/main.h | grep -e 'VERSION_MINOR' | head -n1 | cut -f2)"
-version="$major.$minor"
-if [ -z "$major" -o -z "$minor" ]; then
-	echo "Could not determine version!"
-	exit 1
-fi
-release_name="$project-$version"
-tarball="$release_name.tar.bz2"
-tagname="release-$version"
-tagmsg="$project-$version release"
+hook_testbuild()
+{
+	cd "$1/firmware/cpu-firmware" && make
+	cd "$1/firmware/coproc-firmware" && make
+}
 
-export GIT_DIR="$origin/.git"
-
-cd "$tmpdir"
-rm -Rf "$release_name" "$tarball"
-echo "Creating target directory"
-mkdir "$release_name"
-cd "$release_name"
-echo "git checkout"
-git checkout -f
-
-find "$tmpdir/$release_name" -name makerelease.sh | xargs rm
-find "$tmpdir/$release_name" -name .gitignore | xargs rm
-
-echo "creating tarball"
-cd "$tmpdir"
-tar cjf "$tarball" "$release_name"
-mv "$tarball" "$origin"
-
-echo "running CPU testbuild"
-cd "$tmpdir/$release_name/firmware/cpu-firmware"
-make
-echo "running COPROC testbuild"
-cd "$tmpdir/$release_name/firmware/coproc-firmware"
-make
-
-echo "removing testbuild"
-cd "$tmpdir"
-rm -R "$release_name"
-
-if [ "$do_git_tag" -ne 0 ]; then
-	echo "Tagging GIT"
-	cd "$origin"
-	git tag -m "$tagmsg" -a "$tagname"
-fi
-
-echo
-echo "built release $version"
+project=cnc-control
+makerelease $@
